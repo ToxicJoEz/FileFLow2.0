8c477bcd02f26c20a787dfbd7e6190a9fea8083a
ToxicJoEz <latenightgamer20166@gmail.com>
2026-08-29 19:02:06 +0300

    chore: Make URLs dynamic for production

--- fileflow-backend/src/app.js
+++ fileflow-backend/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 // Security Middlewares
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:5173', // Adjust this to match frontend URL
+  origin: process.env.CLIENT_URL || 'http://localhost:5173', 
   credentials: true
 }));
 
